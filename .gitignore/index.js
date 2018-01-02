7bd4913ca17819ae8bfc05e858586bfcafbf15b3
Viipers <34814704+Viipers@users.noreply.github.com>
2018-01-02 02:59:29 +0100

    Add files via upload

---
+++ .gitignore/index.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+
+var bot = new Discord.Client();
+var prefix = ("/");
+
+
+
+bot.on('ready', () => {
+    bot.user.setPresence({ game: { name: 'Protect and Serve', type : 0}});
+    console.log("Bot Ready");
+});
+
+bot.login('Mzk2MzU5NzI1NzIzMjg3NTYy.DSgUWQ.ipDeoeBHVGPlTTQf5h8ifbe-DmQ');
+
+bot.on('message', message => {    
+
+    var say = message.content.substr(5)
+    if(message.content.startsWith(prefix+'lspd')) {
+            let modRole = message.guild.roles.find("name", "Police");
+
+    if(!message.member.roles.has(modRole.id)) {
+        return message.channel.send(":rotating_light::rotating_light: Intrus detecté :rotating_light::rotating_light: Un administrateur sera averti!").catch(console.log(`** !!! ${message.author.username} : ** a essayé d'utilisé la commande lspd sur ${message.guild.name} voici son message: \`${say}\``));
+
+      } 
+
+    if(!message.guild.roles.exists("name", "Police")) {
+       return  message.channel.send(":rotating_light::rotating_light: Intrus detecté :rotating_light::rotating_light: Un administrateur sera averti!").catch(console.log(`** !!! ${message.author.username} : ** a essayé d'utilisé la commande lspd sur ${message.guild.name} voici son message: \`${say}\``));
+       
+      }
+
+
+           message.channel.send(`${say}`, {disableEveryone: false})
+           message.delete()
+        console.log(`**${message.author.username} : ** a utilisé la commande lspd sur ${message.guild.name} voici son message: \`${say}\``);
+      }
+});
